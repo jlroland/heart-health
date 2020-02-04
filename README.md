2d72571597780af2d4cde39623636a0dc2d60347
Jessica Roland <jlroland415@gmail.com>
2020-02-03 17:19:01 -0800

    Sort through rows and columns to drop

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # heart-health
 
 Angina is often a pre-cursor to cardiovascular disease. Predicting which patients are likely to suffer from angina would help medical professionals gauge cardiovascular risk more effectively. 
+
+While the survey includes data for people of all ages, only data for adults (18+) have been considered here.
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -14,6 +14,48 @@ for file in files:
     df_dict[file.replace('.csv', '')] = pd.read_csv('data/data_2016/{}'.format(file)).set_index('SEQN')
 
 agg_data = pd.concat(df_dict.values(), axis=1)
+agg_data = agg_data[(agg_data['RIDAGEYR']>=18) & (agg_data['RIDSTATR']==2)]  #only considering adults
 
+#demo data
+agg_data.drop(['SDDSRVYR', 'RIDSTATR', 'RIDAGEMN', 'RIDRETH1', 'RIDEXMON', 'RIDEXAGM'], axis=1, inplace=True)
+agg_data.drop(agg_data.loc[:,'RIDEXPRG':'AIALANGA'], axis=1, inplace=True)
+agg_data.drop(agg_data.loc[:,'DMDHRGND':'INDFMPIR'], axis=1, inplace=True)
+
+#BP exam
 agg_data.drop(['PEASCCT1', 'BPAARM', 'BPACSZ', 'BPXPULS', 'BPXPTY', 'BPXML1', 'BPAEN1', 'BPAEN2', 'BPAEN3', 'BPAEN4'], axis=1, inplace=True)
+
+#body measure
+agg_data.drop(['BMIWT', 'BMIRECUM', 'BMXHEAD', 'BMIHEAD', 'BMIHT', 'BMXLEG', 'BMILEG', 'BMXARML', 'BMXARMC', 
+               'BMIARMC', 'BMIWAIST', 'BMXSAD1', 'BMXSAD2', 'BMXSAD3', 'BMXSAD4', 'BMDSADCM'], axis=1, inplace=True)
+
+#lab data
+agg_data.drop(['LBDGLUSI', 'LBDHDDSI', 'LBDTRSI', 'LBDLDLSI', 'LBDHRPLC', 'LBDINSI',
+               'LBDINLC'], axis=1, inplace=True)
+agg_data.drop(['LBDGLTSI', 'GTDSCMMN', 'GTDDR1MN', 'GTDBL2MN', 'GTDDR2MN', 'GTXDRANK', 
+               'GTDCODE'], axis=1, inplace=True)
+
+#diet behavior
+agg_data.drop(agg_data.loc[:, 'DBQ010': 'DBQ424'], axis=1, inplace=True)
+agg_data.drop(agg_data.loc[:,'CBQ596': 'CBQ590'], axis=1, inplace=True)
+
+#health insurance
+agg_data.drop(agg_data.loc[:,'HIQ031A': 'HIQ210'], axis=1, inplace=True)
+
+#income
+agg_data.drop(agg_data.loc[:,'INQ020': 'INQ150'], axis=1, inplace=True)
+agg_data.drop(['INQ320'], axis=1, inplace=True)
+
+#medical conditions
+agg_data.drop(agg_data.loc[:, 'MCQ149':'MCQ180N'], axis=1, inplace=True)
+agg_data.drop(agg_data.loc[:, 'MCQ220':'MCQ240Z'], axis=1, inplace=True)
+agg_data.drop('OSQ230', axis=1, inplace=True)
+
+#physical activity
+agg_data.drop(agg_data.loc[:, 'PAQ722':'PAQ772C'], axis=1, inplace=True)
+
+#cardio questions only asked of people age 40+
+angina = agg_data[(agg_data['CDQ001'] == 1) & (agg_data['CDQ002'] == 1) & (agg_data['CDQ004'] == 1)
+                 & (agg_data['CDQ005'] == 1) & (agg_data['CDQ006'] == 1)
+                 & ((agg_data['CDQ009D'] == 4) | (agg_data['CDQ009E'] == 5))
+                 | ((['CDQ009F'] == 6) & (['CDQ009G'] == 7))]
 
