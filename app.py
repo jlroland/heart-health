dd4fbb73b2cb5cebe6f70d8b5279e46677b90a38
Jessica Roland <jlroland415@gmail.com>
2020-02-10 14:00:56 -0800

    Build out basic skeleton of app without implementing model yet

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 def index():
   return render_template('cardio.html')
 
-@app.route('', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
   user_data = request.json
-  #deconstruct object data into usable variables
-  #define return variables by invoking function for model
-  return jsonify({}) #return variables with values as dictionary
+  age, gender, height, weight, pulse = user_data['age'], user_data['gender'], user_data['height'], user_data['weight'], user_data['pulse']
+  frozen, relative, income, smoke, heaviest = user_data['frozen'], user_data['relative'], user_data['income'], user_data['smoke'], user_data['heaviest']
+  risk = _model_prediction(age, gender, height, weight, pulse, frozen, relative, income, smoke, heaviest)
+  return jsonify({'risk': risk})
+
+def _model_prediction(age, gender, height, weight, pulse, frozen, relative, income, smoke, heaviest):
+  
+  if y_pred==1:
+    return 'high-risk'
+  else:
+    return 'low-risk'
--- static/brapp.py
+++ static/brapp.py
@@ -1,3 +1,42 @@
 from browser import document, ajax
 import json
 
+def get_input_data():
+  age = document['age'].value
+  gender = document['gender'].value
+  height = document['height'].value
+  weight = document['weight'].value
+  pulse = document['pulse'].value
+  frozen = document['frozen'].value
+  relative = document['relative'].value
+  income = document['income'].value
+  smoke = document['smoke'].value
+  heaviest = document['heaviest'].value
+  return {'age': int(age),
+          'gender': ,
+          'height': float(height),
+          'weight': float(weight),
+          'pulse': int(pulse),
+          'frozen': int(frozen),
+          'relative': ,
+          'income': ,
+          'smoke': ,
+          'heaviest': float(heaviest)}
+
+def display_prediction(req):
+  result = json.loads(req.text)
+  document['prediction'].html = f"{result['risk']}"
+
+def send_input_data(data):
+  req = ajax.Ajax()
+  req.bind('complete', display_prediction)
+  req.open('POST', '/predict', True)
+  req.set_header('Content-Type', 'application/json')
+  req.send(json.dumps(data))
+
+
+def click(event):
+  user_info = get_input_data()
+  send_input_data(user_info)
+
+document['predict'].bind('click', click)
