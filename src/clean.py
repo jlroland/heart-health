8352898a718881b113cff31298e69c3cb2293d07
Jessica Roland <jlroland415@gmail.com>
2020-02-26 15:26:32 -0800

    Complete initial cleaning, update msno after image

--- src/clean.py
+++ src/clean.py
@@ -1,10 +1,13 @@
+import numpy as np
 import pandas as pd
+import missingno as msno
 import boto3
 import io
-
 s3 = boto3.client('s3')
+
 obj = s3.get_object(Bucket='health-survey-tables', Key='2016/demo.csv')
-data = pd.read_csv(io.BytesIO(obj['Body'].read())).set_index('SEQN')
+table = pd.read_csv(io.BytesIO(obj['Body'].read())).set_index('SEQN')
+
 key_list = []
 s3objects = s3.list_objects_v2(Bucket='health-survey-tables', StartAfter='2016/' )
 for object in s3objects['Contents']:
@@ -13,8 +16,10 @@ key_list.remove('2016/demo.csv')
 
 for key in key_list:
     response = s3.get_object(Bucket='health-survey-tables', Key=key)
-    data = pd.concat([data, pd.read_csv(io.BytesIO(response['Body'].read())).set_index('SEQN')], axis=1,verify_integrity=True)
-adults = data[(data['RIDAGEYR']>=18) & (data['RIDSTATR']==2)]
+    table = pd.concat([table, pd.read_csv(io.BytesIO(response['Body'].read())).set_index('SEQN')],
+                      axis=1,verify_integrity=True)
+    
+adults = table[(table['RIDAGEYR']>=18) & (table['RIDSTATR']==2)]
 weight_2yrint = adults['WTINT2YR']
 weight_2yrmec = adults['WTMEC2YR']
 adults.drop(['WTINT2YR', 'WTMEC2YR'], axis=1, inplace=True)
@@ -38,29 +43,26 @@ for num in heart_history.index:
 adults.drop(adults.loc[:,'CDQ001':'CDQ010'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'MCQ160B':'MCQ180F'], axis=1, inplace=True)
 
+adults['DMDEDUC2'][((adults['RIDAGEYR']==18) | (adults['RIDAGEYR']==19)) & (adults['DMDEDUC3']==15)] = 0
+sys = adults[['BPXSY1', 'BPXSY2', 'BPXSY3', 'BPXSY4']]
+dia = adults[['BPXDI1', 'BPXDI2', 'BPXDI3', 'BPXDI4']]
+adults['AVGSYS'] = np.nanmean(sys, axis=1)
+adults['AVGDIA'] = np.nanmean(dia, axis=1)
+adults['DBD900'][(adults['DBD895']==0) &(pd.isna(adults['DBD900']))] = 0
+
 #demo
-adults.drop(['SDDSRVYR', 'RIDSTATR', 'RIDAGEMN', 'RIDRETH1', 'RIDEXMON', 'RIDEXAGM', 'DMDCITZN', 'DMDEDUC3'], axis=1, inplace=True)
+adults.drop(['SDDSRVYR', 'RIDSTATR', 'RIDAGEMN', 'RIDRETH1', 'DMQADFC', 'RIDEXMON', 'RIDEXAGM', 'DMDCITZN', 'DMDEDUC3'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'RIDEXPRG':'DMDHSEDU'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'SDMVPSU':'INDFMIN2'], axis=1, inplace=True)
 
-#nutrients
-adults.drop(['DR1EXMER_nutrient1', 'DRABF_nutrient1'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DR1DBIH_nutrient1':'DR1HELP_nutrient1'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DR1STY':'DRQSDT91'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DR1_330Z':'DR1TWS'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DRD350A':'DRD350K'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DRD370A':'DRD370V'], axis=1, inplace=True)
-adults.drop(['DR2EXMER_nutrient2', 'DRABF_nutrient2', 'DR2STY', 'DR2SKY'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DR2DBIH_nutrient2':'DR2HELP_nutrient2'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DR2_330Z':'DR2TWS'], axis=1, inplace=True)
 
 #supps
 adults.drop(['DSDANCNT', 'DSD010'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'DSQTKCAL':'DSQTIODI'], axis=1, inplace=True)
 
 #bp exam
-adults.drop(['BPAEN1', 'BPAEN2', 'BPAEN3', 'BPAEN4'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'PEASCCT1':'BPACSZ'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'BPXPULS':'BPXML1'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'BPXPULS':'BPAEN4'], axis=1, inplace=True)
 
 #body measure
 adults.drop('BMDSADCM', axis=1, inplace=True)
@@ -77,30 +79,20 @@ adults.drop(adults.loc[:,'BPQ060':'BPQ100D'], axis=1, inplace=True)
 
 #diet
 adults.drop(adults.loc[:,'DBQ010':'DBQ073U'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DBQ229':'DBQ424'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'DBQ223A':'DBQ424'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'CBQ596':'CBQ590'], axis=1, inplace=True)
 
 #disability
-adults.drop(['DLQ060', 'DLQ080', 'DLQ110'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DLQ010':'DLQ040'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DLQ140':'DLQ170'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'DLQ010':'DLQ080'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'DLQ110':'DLQ170'], axis=1, inplace=True)
 
-#drugs
-adults.drop('DUQ280', axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ210':'DUQ215U'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ220Q':'DUQ240'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ260':'DUQ270U'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ300':'DUQ320'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ340':'DUQ350U'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'DUQ360':'DUQ430'], axis=1, inplace=True)
 
 #insurance
 adults.drop(adults.loc[:,'HIQ031A':'HIQ210'], axis=1, inplace=True)
 
 #medical conditions
-adults.drop('MCQ300B', axis=1, inplace=True)
-adults.drop(adults.loc[:,'MCQ010':'MCQ053'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'MCD093':'MCQ180N'], axis=1, inplace=True)
+adults.drop(['MCQ160G', 'MCQ300B'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'MCQ010':'MCQ180N'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'MCQ180G':'MCQ240Z'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'MCQ370A':'OSQ230'], axis=1, inplace=True)
 
@@ -117,14 +109,81 @@ adults.drop(adults.loc[:, 'PAQ722':'PAQ772C'], axis=1, inplace=True)
 adults.drop(['SLQ300', 'SLQ310', 'SLQ030', 'SLQ040', 'SLQ050'], axis=1, inplace=True)
 
 #smoking
-adults.drop(adults.loc[:,'SMQ050Q':'SMD641'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'SMD093':'SMD100CO'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'SMD630':'SMQ852U'], axis=1, inplace=True)
-adults.drop(adults.loc[:,'SMQ080':'SMAQUEX2'], axis=1, inplace=True)
+adults.drop(adults.loc[:,'SMQ050Q':'SMAQUEX2'], axis=1, inplace=True)
 
 #weight history
-adults.drop(['WHD130', 'WHQ190', 'WHQ200'], axis=1, inplace=True)
+adults.drop(['WHD110', 'WHD120', 'WHD130', 'WHQ190', 'WHQ200'], axis=1, inplace=True)
 adults.drop(adults.loc[:,'WHD010':'WHD080L'], axis=1, inplace=True)
+
+adults['RIAGENDR'][adults['RIAGENDR']==2] = 0
+adults['DMQMILIZ'][adults['DMQMILIZ']==2] = 0
+adults['DMDBORN4'][adults['DMDBORN4']>1] = 0
+adults['BPQ020'][adults['BPQ020'] > 1] = 0
+adults['BPQ080'][adults['BPQ080'] > 1] = 0
+adults['DBQ700'][adults['DBQ700']==9] = 3
+adults['DBQ197'][adults['DBQ197']==4] = np.array([3, 3, 2, 2, 3, 3, 2, 2, 3, 3, 3, 2, 3, 3, 2, 2, 3, 2])
+#adults['DBQ197'] hard-coded with result of np.random.randint
+adults['DBD895'][adults['DBD895'] == 5555] = 22
+adults['DBD895'][adults['DBD895'] == 9999] = round(adults['DBD895'][adults['DBD895'] != 9999].mean())
+adults['HIQ011'][adults['HIQ011']>1] = 0
+adults['MCQ365A'][adults['MCQ365A'] > 1] = 0
+adults['MCQ365B'][adults['MCQ365B'] > 1] = 0
+adults['MCQ365C'][adults['MCQ365C'] > 1] = 0
+adults['MCQ365D'][adults['MCQ365D'] > 1] = 0
+adults['PAQ605'][adults['PAQ605'] > 1] = 0
+adults['PAQ620'][adults['PAQ620'] > 1] = 0
+adults['PAQ635'][adults['PAQ635'] > 1] = 0
+adults['PAQ650'][adults['PAQ650'] > 1] = 0
+adults['PAQ665'][adults['PAQ665'] > 1] = 0
+adults['PAQ710'][adults['PAQ710'] == 8] = 0
+adults['PAQ710'][(adults['PAQ710'] == 99) | (adults['PAQ710'] == 77)] = round(adults['PAQ710'][(adults['PAQ710'] != 99) & (adults['PAQ710'] != 77)].mean())
+adults['PAQ715'][adults['PAQ715'] == 8] = 0
+adults['PAQ715'][(adults['PAQ715'] == 99) | (adults['PAQ715'] == 77)] = round(adults['PAQ715'][(adults['PAQ715'] != 99) & (adults['PAQ715'] != 77)].mean())
+adults['SLQ120'][adults['SLQ120'] < 3] = 0
+adults['SLQ120'][adults['SLQ120'] >= 3] = 1
+adults['SMQ020'][adults['SMQ020'] > 1] = 0
+adults['DSDCOUNT'][(adults['DSDCOUNT'] == 99) | (adults['DSDCOUNT'] == 77)] = round(adults['DSDCOUNT'][(adults['DSDCOUNT'] != 99) & (adults['DSDCOUNT'] != 77)].mean())
+adults['DSD010AN'][adults['DSD010AN'] > 1] = 0
+adults['WHQ225'][adults['WHQ225'] >= 5] = 0
+adults['WHQ225'][adults['WHQ225'] < 5] = 1
+adults['DMDYRSUS'][adults['DMDYRSUS'] >= 4] = 1
+adults['DMDYRSUS'][adults['DMDYRSUS'] < 4] = 0
+adults['DMDMARTL'][(adults['DMDMARTL'] <= 4) | (adults['DMDMARTL'] == 77)] = 1
+adults['DMDMARTL'][(adults['DMDMARTL'] > 4) | (pd.isna(adults['DMDMARTL']))] = 0
+#adults['DMDMARTL'] missing values were all individuals under age 20, who were not asked the question
+adults['DMDEDUC2'][(adults['DMDEDUC2'] <= 3) | (adults['DMDEDUC2'] == 9)] = 0
+adults['DMDEDUC2'][adults['DMDEDUC2'] > 3] = 1
+adults['ALQ101'][adults['ALQ101']==9] = np.nan
+adults['ALQ141Q'][(adults['ALQ141Q'] == 777) | (adults['ALQ141Q'] == 999)] = np.nan
+adults['ALQ141Q'][(adults['ALQ101']==2) & (pd.isna(adults['ALQ141Q']))] = 0
+adults['BPQ030'][(adults['BPQ030'] > 1) | (pd.isna(adults['BPQ030']))] = 0
+adults['DBD900'][(adults['DBD900'] == 9999) | (adults['DBD900'] == 7777)] = np.nan
+adults['DBD900'][pd.isna(adults['DBD900'])] = round(np.nanmean(adults['DBD900']))
+adults['DBD905'][adults['DBD905'] == 6666] = 91
+adults['DBD905'][(adults['DBD905'] == 9999) | (adults['DBD905'] == 7777)] = np.nan
+adults['DBD905'][pd.isna(adults['DBD905'])] = round(np.nanmean(adults['DBD905']))
+adults['DBD910'][(adults['DBD910'] == 9999) | (adults['DBD910'] == 7777)] = np.nan
+adults['DBD910'][pd.isna(adults['DBD910'])] = round(np.nanmean(adults['DBD910']))
+adults['DLQ100'][(adults['DLQ100'] >= 3) | pd.isna(adults['DLQ100'])] = 0
+adults['DLQ100'][adults['DLQ100'] < 3] = 1
+adults['MCQ300A'][adults['MCQ300A'] == 2] = 0
+adults['MCQ300A'][(adults['MCQ300A'] == 9) | (adults['MCQ300A'] == 7)] = np.nan
+adults['MCQ300C'][adults['MCQ300C'] == 2] = 0
+adults['MCQ300C'][(adults['MCQ300C'] == 9) | (adults['MCQ300C'] == 7)] = np.nan
+adults['OCQ180'][(adults['OCQ180'] == 99999) | (adults['OCQ180'] == 77777)] = np.nan
+adults['SLD012'][pd.isna(adults['SLD012'])] = round(np.nanmean(adults['SLD012']))
+adults['SMD030'][pd.isna(adults['SMD030'])] = 0
+adults['SMD030'][(adults['SMD030'] == 999) | (adults['SMD030'] == 777)] = round(adults['SMD030'][(adults['SMD030'] != 999) & (adults['SMD030'] != 777) & (adults['SMD030'] != 0)].mean())
+adults['SMQ040'][adults['SMQ040'] == 2] = 1
+adults['SMQ040'][(adults['SMQ040'] > 2) | (pd.isna(adults['SMQ040']))] = 0
+adults['WHD140'][(adults['WHD140'] == 9999) | (adults['WHD140'] == 7777)] = np.nan
+adults['WHQ150'][adults['WHQ150'] == 99999] = np.nan
+
+race = pd.get_dummies(adults['RIDRETH3'], prefix='race')
+diet_health = pd.get_dummies(adults['DBQ700'], prefix='diet_health')
+milk = pd.get_dummies(adults['DBQ197'], prefix='milk')
+data = pd.concat([adults, race, diet_health, milk], axis=1)
+data.drop(['RIDRETH3', 'DBQ700', 'DBQ197'], axis=1, inplace=True)
 
 '''
 initial drop--
