432178238abb79807004f991a51e0d6ab60f0ae8
Jessica Roland <jlroland415@gmail.com>
2020-02-28 09:24:10 -0800

    Changed gender input to toggle selection

--- app.py
+++ app.py
@@ -11,44 +11,44 @@ app = Flask(__name__)
 def index():
   return render_template('cardio.html')
 
+pickle_file = open('model.pickle', 'rb')
+model = pickle.load(pickle_file)
+
 @app.route('/predict', methods=['POST'])
 def predict():
   user_data = request.json
   age, gender, height, weight, pulse = user_data['age'], user_data['gender'], user_data['height'], user_data['weight'], user_data['pulse']
-  frozen, relative, income, smoke, heaviest = user_data['frozen'], user_data['relative'], user_data['income'], user_data['smoke'], user_data['heaviest']
-  risk = _model_prediction(age, gender, height, weight, pulse, frozen, relative, income, smoke, heaviest)
+  frozen, relative, smoke, heaviest = user_data['frozen'], user_data['relative'], user_data['smoke'], user_data['heaviest']
+  risk = _model_prediction(age, gender, height, weight, pulse, frozen, relative, smoke, heaviest)
   return jsonify({'risk': risk})
 
-def _model_prediction(age, gender, height, weight, pulse, frozen, relative, income, smoke, heaviest):
-  gender, bmi, relative, income, smoke = _clean_data(gender, height, weight, relative, income, smoke)
-  X = np.array([age, gender, bmi, pulse, frozen, relative, income, smoke, heaviest]).reshape(1, -1)
+def _model_prediction(age, gender, height, weight, pulse, frozen, relative, smoke, heaviest):
+  gender, bmi, relative, smoke = _clean_data(gender, height, weight, relative, smoke)
+  X = np.array([age, gender, bmi, pulse, frozen, relative, smoke, heaviest]).reshape(1, -1)
   scaled = MinMaxScaler()
   X_scaled = scaled.fit_transform(X)
-  pickle_file = open('model.pickle', 'rb')
-  model = pickle.load(pickle_file)
   y_hat = model.predict_proba(X_scaled)[:,1]
   if y_hat > 0.5:
     return 'high-risk'
   else:
     return 'low-risk'
 
-def _clean_data(gender, height, weight, relative, income, smoke):
-  gender, relative, income, smoke = gender.lower(), relative.lower(), income.lower(), smoke.lower()
-  if gender=='m':
-    gender=1
-  else:
-    gender=0
+def _clean_data(height, weight, relative, smoke):
+  relative, income, smoke = gender.lower(), relative.lower(), smoke.lower()
+#   if gender=='m':
+#     gender=1
+#   else:
+#     gender=0
   if relative=='y':
     relative=1
   else:
     relative=0
-  if income=='y':
-    income=1
-  else:
-    income=0
   if smoke=='y':
     smoke=1
   else:
     smoke=0
   bmi = (weight*0.453592)/(height*0.0254)**2
-  return gender, bmi, relative, income, smoke
+  return bmi, relative, smoke
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8000, debug=True)
